5ee1304e195ad8f0949c903d0d06e9eae1e0b1bf
Jason Lo <mr.lo.mhs@gmail.com>
2018-01-20 15:20:43 -0800

    add constant definitions

--- gambling/models.rb
+++ gambling/models.rb
@@ -8,16 +8,33 @@ end
 class CardGame < ActiveRecord::Base
   has_many :acquisitions, as: :game
   has_many :casinos, through: :acquisitions
+
+  TYPE_BLACKJACK = 0
+  TYPE_POKER = 1
 end
 
 class BettingGame < ActiveRecord::Base
   has_many :acquisitions, as: :game
   has_many :casinos, through: :acquisitions
+
+  TYPE_HORSE_RACE = 0
+  TYPE_ELECTION = 1
+
+  CLASSIFICATION_LOCAL = 0
+  CLASSIFICATION_REGIONAL = 1
+  CLASSIFICATION_NATIONAL = 2
+  CLASSIFICATION_INTERNATIONAL = 3
+
+  LEGALITY_LEGAL = 0
+  LEGALITY_ILLEGAL = 1
+  LEGALITY_UNCLEAR = 2
 end
 
 class DiceGame < ActiveRecord::Base
   has_many :acquisitions, as: :game
   has_many :casinos, through: :acquisitions
+
+  TYPE_CRAPS = 0
 end
 
 class Acquisition < ActiveRecord::Base
